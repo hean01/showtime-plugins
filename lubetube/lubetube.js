00aa5328c9827ba7fc8125bd87787d8784a05bb6
Henrik Andersson <hean01@users.sourceforge.net>
2012-10-19 23:24:51 +0200

    Strip hardcoded categories and parse list online.

--- lubetube/lubetube.js
+++ lubetube/lubetube.js
@@ -42,65 +42,19 @@
 	});
     }
 
-    function addDir(page, name, url, image)
-    {
-	page.appendItem(PREFIX + "category:" + name + ":" + url, "directory", {
-	    title: name
-	});
-    }
-
     function categories(page) {
-	addDir(page, "Most Recent", "/", 1, "")
-        addDir(page, "Amateur", "/search/cat/amateur", 1, "")
-        addDir(page, "Anal", "/search/cat/anal", 1, "")
-        addDir(page, "Asian", "/search/cat/asian", 1, "")
-        addDir(page, "Ass", "/search/cat/asslicking", 1, "")
-        addDir(page, "BBW", "/search/cat/bbw", 1, "")
-        addDir(page, "Big Butt", "/search/cat/bigbutt", 1, "")
-        addDir(page, "Big Cock", "/search/cat/bigcock", 1, "")
-        addDir(page, "Big Tits", "/search/cat/bigtits", 1, "")
-        addDir(page, "Blonde", "/search/cat/blonde", 1, "")
-        addDir(page, "Blowjob", "/search/cat/blowjob", 1, "")
-        addDir(page, "Bondage", "/search/cat/bondage", 1, "")
-        addDir(page, "Creampie", "/search/cat/creampie", 1, "")
-        addDir(page, "Cumshot", "/search/cat/cumshot", 1, "")
-        addDir(page, "Cum Swapping", "/search/cat/cumswapping", 1, "")
-        addDir(page, "Double Penetration", "/search/cat/doublepenetration", 1, "")
-        addDir(page, "Ebony", "/search/cat/ebony", 1, "")
-        addDir(page, "Facial", "/search/cat/facial", 1, "")
-        addDir(page, "Fetish", "/search/cat/fetish", 1, "")
-        addDir(page, "Fingering", "/search/cat/fingering", 1, "")
-        addDir(page, "Fisting", "/search/cat/fisting", 1, "")
-        addDir(page, "Foot Fetish", "/search/cat/footfetish", 1, "")
-        addDir(page, "Footjob", "/search/cat/footjob", 1, "")
-        addDir(page, "Fucking Machine", "/search/cat/fuckingmachine", 1, "")
-        addDir(page, "Gagging", "/search/cat/gagging", 1, "")
-        addDir(page, "Gangbang", "/search/cat/gangbang", 1, "")
-        addDir(page, "Group Sex", "/search/cat/groupsex", 1, "")
-        addDir(page, "Handjob", "/search/cat/handjob", 1, "")
-        addDir(page, "Hardcore", "/search/cat/hardcore", 1, "")
-        addDir(page, "Insertion", "/search/cat/insertion", 1, "")
-        addDir(page, "Interracial", "/search/cat/interracial", 1, "")
-        addDir(page, "Latex", "/search/cat/latex", 1, "")
-        addDir(page, "Latina", "/search/cat/latina", 1, "")
-        addDir(page, "Lesbian", "/search/cat/lesbian", 1, "")
-        addDir(page, "Masturbation", "/search/cat/masturbation", 1, "")
-        addDir(page, "MILF", "/search/cat/milf", 1, "")
-        addDir(page, "Party", "/search/cat/party", 1, "")
-        addDir(page, "Pornstar", "/search/cat/pornstar", 1, "")
-        addDir(page, "POV", "/search/cat/pov", 1, "")
-        addDir(page, "Public", "/search/cat/public", 1, "")
-        addDir(page, "Reality", "/search/cat/reality", 1, "")
-        addDir(page, "Redhead", "/search/cat/redhead", 1, "")
-        addDir(page, "Solo", "/search/cat/solo", 1, "")
-        addDir(page, "Spanking", "/search/cat/spanking", 1, "")
-        addDir(page, "Squirting", "/search/cat/squirting", 1, "")
-        addDir(page, "Striptease", "/search/cat/striptease", 1, "")
-        addDir(page, "Teen", "/search/cat/teen", 1, "")
-        addDir(page, "Threesome", "/search/cat/threesome", 1, "")
-        addDir(page, "Toys", "/search/cat/toys", 1, "")
-        addDir(page, "Tranny", "/search/cat/tranny", 1, "")
-        addDir(page, "Voyeur", "/search/cat/voyeur", 1, "")
+	var response = showtime.httpGet(BASE_URL+"/categories");
+	var re = /<a href="http:\/\/lubetube.com([^\s]+)page=1"><img width="[0-9]+" height="[0-9]+" alt="[^"]+" class="main_gallery" src="([^\s]+)" title="([^"]+)" \/><\/a><br \/>/g;
+
+	var match = re.exec(response);
+	while(match)
+	{
+	    page.appendItem(PREFIX + "category:" + match[3] + ":" + match[1], "directory", {
+		title: match[3],
+		icon: match[2]
+	    });
+	    var match = re.exec(response);
+	}
     }
 
     function index(page, url) {
