ce3c463f82065bd68d802d5bd9c19afe1d9bfe0b
lprot <lprot@list.ru>
2012-10-26 18:37:22 +0300

    v1.22 alot of changes:   plugins/redtube.zip 	v1.22:   redtube/plugin.json 	v1.22:   redtube/redtube.js

--- redtube/redtube.js
+++ redtube/redtube.js
@@ -20,42 +20,44 @@
 (function(plugin) {
 
     var PREFIX = 'redtube';
+    var BASE_URL = 'http://www.redtube.com';
 
     var logo = plugin.path + "logo.png";
 
     function fix_entity(doc) {
-	doc = doc.replace(/\&Acirc;\&iexcl;/g,'\u00a1');
-	doc = doc.replace(/\&Acirc;\&deg;/g,'\u00b0');
-	doc = doc.replace(/\&Atilde;\&nbsp;/g,'\u00e0');
-	doc = doc.replace(/\&Atilde;\&iexcl;/g,'\u00e1');
-	doc = doc.replace(/\&Atilde;\&cent;/g,'\u00e2');
-	doc = doc.replace(/\&Atilde;\&pound;/g,'\u00e3');
-	doc = doc.replace(/\&Atilde;\&curren;/g,'\u00e4');
-	doc = doc.replace(/\&Atilde;\&yen;/g,'\u00e5');
-	doc = doc.replace(/\&Atilde;\&sect;/g,'\u00e7');
-	doc = doc.replace(/\&Atilde;\&uml;/g,'\u00e8');
-	doc = doc.replace(/\&Atilde;\&copy;/g,'\u00e9');
-	doc = doc.replace(/\&Atilde;\&ordf;/g,'\u00ea');
-	doc = doc.replace(/\&Atilde;\&laquo;/g,'\u00eb');
-	doc = doc.replace(/\&Atilde;\&reg;/g,'\u00ee');
-	doc = doc.replace(/\&Atilde;\&shy;/g,'\u00ed');
-	doc = doc.replace(/\&Atilde;\&plusmn;/g,'\u00f1');
-	doc = doc.replace(/\&Atilde;\&sup3;/g,'\u00f3');
-	doc = doc.replace(/\&Atilde;\&acute;/g,'\u00f4');
-	doc = doc.replace(/\&Atilde;\&micro;/g,'\u00f5');
-	doc = doc.replace(/\&Atilde;\&para;/g,'\u00f6');
-	doc = doc.replace(/\&Atilde;\&ordm;/g,'\u00fa');
-	doc = doc.replace(/\&Atilde;\&frac14;/g,'\u00fc');
-	doc = doc.replace(/\&quot;/g,'\"');
-	doc = doc.replace(/\&amp;/g,'&');
-	doc = doc.replace(/\&gt;/g,'>');
-	doc = doc.replace(/\&lt;/g,'<');
-	doc = doc.replace(/\&#039;/g,'\'');
-	doc = doc.replace(/\&#39;/g,'\'');
-	if (doc.indexOf('\&Atilde;')!=-1) showtime.message(doc,true,false);
-	if (doc.indexOf('\&Acirc;')!=-1) showtime.message(doc,true,false);
-	return doc;
-    } 
+        doc = doc.replace(/\&Acirc;\&iexcl;/g, '\u00a1');
+        doc = doc.replace(/\&Acirc;\&ordf;/g, '\u00aa');
+        doc = doc.replace(/\&Acirc;\&deg;/g, '\u00b0');
+        doc = doc.replace(/\&Atilde;\&nbsp;/g, '\u00e0');
+        doc = doc.replace(/\&Atilde;\&iexcl;/g, '\u00e1');
+        doc = doc.replace(/\&Atilde;\&cent;/g, '\u00e2');
+        doc = doc.replace(/\&Atilde;\&pound;/g, '\u00e3');
+        doc = doc.replace(/\&Atilde;\&curren;/g, '\u00e4');
+        doc = doc.replace(/\&Atilde;\&yen;/g, '\u00e5');
+        doc = doc.replace(/\&Atilde;\&sect;/g, '\u00e7');
+        doc = doc.replace(/\&Atilde;\&uml;/g, '\u00e8');
+        doc = doc.replace(/\&Atilde;\&copy;/g, '\u00e9');
+        doc = doc.replace(/\&Atilde;\&ordf;/g, '\u00ea');
+        doc = doc.replace(/\&Atilde;\&laquo;/g, '\u00eb');
+        doc = doc.replace(/\&Atilde;\&reg;/g, '\u00ee');
+        doc = doc.replace(/\&Atilde;\&shy;/g, '\u00ed');
+        doc = doc.replace(/\&Atilde;\&plusmn;/g, '\u00f1');
+        doc = doc.replace(/\&Atilde;\&sup3;/g, '\u00f3');
+        doc = doc.replace(/\&Atilde;\&acute;/g, '\u00f4');
+        doc = doc.replace(/\&Atilde;\&micro;/g, '\u00f5');
+        doc = doc.replace(/\&Atilde;\&para;/g, '\u00f6');
+        doc = doc.replace(/\&Atilde;\&ordm;/g, '\u00fa');
+        doc = doc.replace(/\&Atilde;\&frac14;/g, '\u00fc');
+        doc = doc.replace(/\&quot;/g, '\"');
+        doc = doc.replace(/\&amp;/g, '&');
+        doc = doc.replace(/\&gt;/g, '>');
+        doc = doc.replace(/\&lt;/g, '<');
+        doc = doc.replace(/\&#039;/g, '\'');
+        doc = doc.replace(/\&#39;/g, '\'');
+        if (doc.indexOf('\&Atilde;') != -1) showtime.message(doc, true, false);
+        if (doc.indexOf('\&Acirc;') != -1) showtime.message(doc, true, false);
+        return doc;
+    }
 
     function setPageHeader(page, title) {
         if (page.metadata) {
@@ -86,7 +88,9 @@
 
     function parental_control(page) {
         if (service.parental) {
-            var pin = plugin.cacheGet("PIN", "Value");
+            try {
+                var pin = plugin.cacheGet("PIN", "Value");
+            } catch (err) {}
             if ((pin) && (service.pin == pin.pin)) return true;
             var res = showtime.textDialog('Redtube parental control. Enter PIN: ', true, true);
             plugin.cachePut("PIN", "Value", {
@@ -107,38 +111,155 @@
         };
 
         setPageHeader(page, 'Redtube - Home Page');
+        page.loading = false;
         page.appendItem(PREFIX + ':categories', 'directory', {
-            title: 'Categories'
+            title: 'Categories',
+            icon: logo
+        });
+        page.appendItem(PREFIX + ':categoriesAPI', 'directory', {
+            title: 'Categories (API)',
+            icon: logo
         });
         page.appendItem(PREFIX + ':tags', 'directory', {
-            title: 'Tags'
+            title: 'Tags',
+            icon: logo
+        });
+        page.appendItem(PREFIX + ':pornstars', 'directory', {
+            title: 'Pornstar Directory',
+            icon: logo
         });
         page.appendItem(PREFIX + ':stars', 'directory', {
-            title: 'Stars'
+            title: 'Stars',
+            icon: logo
         });
-        page.loading = false;
     };
+
+    function index_pornstars(page, url) {
+        var offset = 0;
+        if (page.metadata) var title = page.metadata.title;
+        var response = showtime.httpGet(url).toString();
+        function loader() {
+	    var items = 0;
+            var re = /class="pornStar">[\S\s]*?<a href="([^"]+)" title="([^"]+)[\S\s]*?src="([^"]+)[\S\s]*?class="videosCount">([\d^\s]+)\s/g;
+            var match = re.exec(response);
+            while (match) {
+                page.appendItem(PREFIX + ":category:" + escape(match[1]) + ":" + escape(match[2]), "video", {
+                    title: new showtime.RichText(match[2] + '<font color="6699CC"> (' + match[4] + ')</font>'),
+                    icon: match[3]
+                });
+                offset++;
+		items++;
+		if (items > 50) {
+			break;			
+		};
+                match = re.exec(response);
+            }
+
+            re = /categoryHeading">[\S\s^]*?([\d].*)\)/;
+            match = re.exec(response);
+            if (match) {
+                page.entries = match[1].replace(",", "");
+                if (page.metadata) page.metadata.title = title + " - found (" + match[1].replace(",", "") + ") videos"
+            }
+            return offset < page.entries;
+        }
+        loader();
+        page.loading = false;
+        page.paginator = loader;
+    }
+
+    function index(page, url) {
+        var offset = 0;
+        if (page.metadata) var title = page.metadata.title;
+        function loader() {
+            var p = Math.floor(1 + (offset / 50));
+            var response = showtime.httpGet(url + "?page=" + p).toString();
+	    var re = /><b>Next<\/b>/;
+            if ((offset > 0) && (!re.exec(response))) return false;
+            re = /class="video">[\S\s]*?<a href="([^"]+)" title="([^"]+)[\S\s]*?src="([^"]+)[\S\s]*?class="d">([^\<]+)[\S\s]*?;">[\S\s]*?([0-9^\<].*)[\S\s]*?;">[\S\s]*?([0-9].*)\sviews/g;
+            var match = re.exec(response);
+            while (match) {
+                page.appendItem(PREFIX + ":play:" + escape(match[1]) + ":" + escape(match[2]), "video", {
+                    title: new showtime.RichText(match[2] + '<font color="6699CC"> (' + match[4] + ')</font>'),
+                    icon: match[3],
+                    description: new showtime.RichText('<font color="6699CC">Views: </font>' + match[6]),
+                    genre: 'Adult',
+                    duration: match[4],
+                    rating: +(match[5]) * 20
+                });
+                match = re.exec(response);
+                offset++;
+            }
+
+            re = /categoryHeading">[\S\s^]*?([\d].*)\)/;
+            match = re.exec(response);
+            if (match) {
+                page.entries = match[1].replace(",", "");
+                if (page.metadata) page.metadata.title = title + " - found (" + match[1].replace(",", "") + ") videos"
+            }
+            return offset < page.entries;
+        }
+        loader();
+        page.loading = false;
+        page.paginator = loader;
+    }
+
+    // Enter Pornstar Directory
+    plugin.addURI(PREFIX + ":pornstar", function(page) {
+        setPageHeader(page, 'Redtube - Pornstar Directory');
+        index_pornstars(page, BASE_URL + '/pornstar');
+    });
+
+
+    // Enter category
+    plugin.addURI(PREFIX + ":category:(.*):(.*)", function(page, uri, name) {
+        setPageHeader(page, unescape(name));
+        index(page, BASE_URL + uri);
+    });
+
+    plugin.addURI(PREFIX + ":categoriesAPI", function(page) {
+        setPageHeader(page, 'Redtube - Categories');
+        //Getting pictures
+        var response = showtime.httpGet(BASE_URL + "/channels");
+        page.loading = false;
+        var re = /class="video">[\S\s]*?<a href="([^"]+)" title="([^"]+)[\S\s]*?src="([^"]+)[\S\s]*?numberVideos">[\S\s]*?([0-9].*)\sVideos/g;
+        var match = re.exec(response);
+        var jsonobj = showtime.JSONDecode(showtime.httpGet("http://api.redtube.com/?data=redtube.Categories.getCategoriesList&output=json").toString());
+        page.loading = false;
+        for (var i in jsonobj.categories) {
+            page.appendItem(PREFIX + ':search:category=' + escape(jsonobj.categories[i].category), 'video', {
+                title: jsonobj.categories[i].category,
+                icon: match[3]
+            });
+            match = re.exec(response);
+        }
+    });
 
     plugin.addURI(PREFIX + ":categories", function(page) {
         setPageHeader(page, 'Redtube - Categories');
-        var jsonobj = showtime.JSONDecode(showtime.httpGet("http://api.redtube.com/?data=redtube.Categories.getCategoriesList&output=json").toString());
-        for (var i in jsonobj.categories) {
-            page.appendItem(PREFIX + ':search:category=' + escape(jsonobj.categories[i].category), 'directory', {
-                title: jsonobj.categories[i].category
-            })
-        }
+        var response = showtime.httpGet(BASE_URL + "/channels");
         page.loading = false;
+        var re = /class="video">[\S\s]*?<a href="([^"]+)" title="([^"]+)[\S\s]*?src="([^"]+)[\S\s]*?numberVideos">[\S\s]*?([0-9].*)\sVideos/g;
+        var match = re.exec(response);
+        while (match) {
+            page.appendItem(PREFIX + ":category:" + match[1] + ":" + escape(match[2]), "video", {
+                title: new showtime.RichText(match[2] + '<font color="6699CC"> (</font>' + match[4].replace(',', '') + '<font color="6699CC">)</font>'),
+                icon: match[3]
+            });
+            match = re.exec(response);
+        }
     });
 
     plugin.addURI(PREFIX + ":tags", function(page) {
         setPageHeader(page, 'Redtube - Tags');
         var jsonobj = showtime.JSONDecode(showtime.httpGet("http://api.redtube.com/?data=redtube.Tags.getTagList&output=json").toString());
+        page.loading = false;
         for (var i in jsonobj.tags) {
             page.appendItem(PREFIX + ':search:tags[]=' + escape(jsonobj.tags[i].tag.tag_name), 'directory', {
-                title: jsonobj.tags[i].tag.tag_name
+                title: jsonobj.tags[i].tag.tag_name,
+                icon: logo
             })
         }
-        page.loading = false;
     });
 
     plugin.addURI(PREFIX + ":stars", function(page) {
@@ -154,7 +275,8 @@
             var itemCounter = 0;
             while ((itemCounter < 20) && (offset < starCounter)) {
                 page.appendItem(PREFIX + ':search:stars[]=' + escape(jsonobj.stars[offset].star.star_name), 'directory', {
-                    title: jsonobj.stars[offset].star.star_name
+                    title: jsonobj.stars[offset].star.star_name,
+                    icon: logo
                 });
                 itemCounter++;
                 offset++;
@@ -168,7 +290,7 @@
     });
 
     function getTimestamp(str) {
-        if (!str) return false;
+        if (!str) return 0;
         var d = str.match(/\d+/g); // extract date parts
         return +Date.UTC(d[0], d[1] - 1, d[2], d[3], d[4], d[5]) / 1000;
     }
@@ -185,9 +307,9 @@
             if (!jsonobj.videos) return -1;
             setPageHeader(page, 'Redtube - Search (found ' + jsonobj.count + ' videos)');
             for (var i in jsonobj.videos) {
-                var description = '<font size="4" color="6699CC">Video ID: </font><font size="4">' + jsonobj.videos[i].video.video_id + '</font> ';
-                description += '<font size="4" color="6699CC">Views: </font><font size="4">' + jsonobj.videos[i].video.views + '</font> ';
-                description += '<font size="4" color="6699CC">Ratings: </font><font size="4">' + jsonobj.videos[i].video.ratings + '</font>\n';
+                var description = '<font color="6699CC">Video ID: </font>' + jsonobj.videos[i].video.video_id + ' ';
+                description += '<font color="6699CC">Views: </font>' + jsonobj.videos[i].video.views + ' ';
+                description += '<font color="6699CC">Ratings: </font>' + jsonobj.videos[i].video.ratings + '\n';
 
                 var stars = 0;
                 if (jsonobj.videos[i].video.stars) {
@@ -197,7 +319,7 @@
                         stars += jsonobj.videos[i].video.stars[j].star_name;
                     };
                 };
-                if (stars) description += '<font size="4" color="6699CC">Starring: </font><font size="4">' + stars + '.</font>\n';
+                if (stars) description += '<font color="6699CC">Starring: </font>' + stars + '.\n';
 
                 var tags = 0;
                 if (jsonobj.videos[i].video.tags) {
@@ -207,17 +329,16 @@
                         tags += jsonobj.videos[i].video.tags[j].tag_name;
                     };
                 };
-                if (tags) description += '<font size="4" color="6699CC">Tags: </font><font size="4">' + tags + '.</font>';
+                if (tags) description += '<font color="6699CC">Tags: </font>' + tags + '.';
 
-                if (jsonobj.videos[i].video.title) 
-                	var title = jsonobj.videos[i].video.title
-		else {
-			var title = showtime.JSONDecode(showtime.httpGet("http://api.redtube.com/?data=redtube.Videos.getVideoById&video_id="+jsonobj.videos[i].video.video_id + "&output=json&thumbsize=none").toString());
-			title = title.video.title
-		}
-		title = showtime.entityDecode(fix_entity(title));
+                if (jsonobj.videos[i].video.title) var title = jsonobj.videos[i].video.title
+                else {
+                    var title = showtime.JSONDecode(showtime.httpGet("http://api.redtube.com/?data=redtube.Videos.getVideoById&video_id=" + jsonobj.videos[i].video.video_id + "&output=json&thumbsize=none").toString());
+                    title = title.video.title
+                }
+                title = showtime.entityDecode(fix_entity(title));
                 page.appendItem(PREFIX + ":play:" + jsonobj.videos[i].video.video_id + ":" + escape(title), "video", {
-                    title: new showtime.RichText(title + '<font size="4" color="6699CC"> ( ' + jsonobj.videos[i].video.duration + ' )</font>'),
+                    title: new showtime.RichText(title + '<font color="6699CC"> ( ' + jsonobj.videos[i].video.duration + ' )'),
                     rating: +(jsonobj.videos[i].video.rating) * 20,
                     icon: jsonobj.videos[i].video.default_thumb,
                     duration: jsonobj.videos[i].video.duration,
@@ -242,16 +363,17 @@
     });
 
     plugin.addURI(PREFIX + ":play:(.*):(.*)", function(page, video_id, title) {
-        var re = /flv_h264_url\=(.*?)\&/i;
+        page.loading = false;
+        var re = /flv_h264_url=(.*?)&/;
         var m = re.exec(showtime.httpGet("http://www.redtube.com/" + video_id).toString());
         page.type = "video";
         page.source = "videoparams:" + showtime.JSONEncode({
             title: unescape(title),
+            canonicalUrl: PREFIX + ":play:" + video_id + ":" + title,
             sources: [{
                 url: unescape(m[1])
             }]
         });
-        page.loading = false;
     });
 
     plugin.addURI(PREFIX + ":start", startPage);
@@ -265,7 +387,7 @@
             return;
         };
         try {
-	    query=query.replace(/\s/g,'\+');
+            query = query.replace(/\s/g, '\+');
             preparePage(page, escape("http://api.redtube.com/?data=redtube.Videos.searchVideos&output=json&thumbsize=none&search="), escape(query));
         } catch (err) {
             showtime.trace('Redtube - Searcher error: ' + err)
