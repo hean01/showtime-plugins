c0f5f511882f5b54b4e89e2c92b70be09471fffd
lprot <lprot@list.ru>
2013-01-20 22:31:17 +0200

    many important changes:   ex_ua/ex_ua.js 	ver 0.4:   ex_ua/plugin.json 	ver 0.4:   plugins/ex_ua.zip

--- ex_ua/ex_ua.js
+++ ex_ua/ex_ua.js
@@ -84,6 +84,18 @@
             title: match[6],
             icon: logo
         });
+        page.appendItem(PREFIX + ':index:/top', 'directory', {
+            title: 'Самое популярное (Most popular)',
+            icon: logo
+        });
+        page.appendItem(PREFIX + ':index:/top/1', 'directory', {
+            title: 'Самое обсуждаемое (Most discussed)',
+            icon: logo
+        });
+        page.appendItem(PREFIX + ':index:/top/2', 'directory', {
+            title: 'Самое рекомендуемое (Most recommended)',
+            icon: logo
+        });
     };
 
     // Play megogo links
@@ -92,11 +104,11 @@
         var match = re.exec(url);
         var sign = showtime.md5digest('video=' + match[1] + '1e5774f77adb843c');
         sign = showtime.JSONDecode(showtime.httpGet('http://megogo.net/p/info?video=' + match[1] + '&sign=' + sign + '_samsungtv'));
-	if (!sign.src) {
-		page.loading=false;
-		showtime.message("Error: This video is not available in your region :(", true, false);
-		return;
-	}
+        if (!sign.src) {
+            page.loading = false;
+            showtime.message("Error: This video is not available in your region :(", true, false);
+            return;
+        }
         page.type = "video";
         page.source = "videoparams:" + showtime.JSONEncode({
             title: unescape(sign.title),
@@ -109,7 +121,7 @@
     });
 
     function getType(type) {
-	type = type.toLowerCase();
+        type = type.toLowerCase();
         switch (type) {
             case "mkv":
             case "avi":
@@ -121,11 +133,14 @@
             case "mpeg":
             case "vob":
             case "iso":
+            case "m4v":
+            case "wmv":
                 return "video";
             case "jpg":
             case "jpeg":
             case "png":
             case "bmp":
+            case "gif":
                 return "image";
             case "mp3":
             case "flac":
@@ -134,8 +149,8 @@
             case "aac":
             case "m4a":
             case "ape":
-	    case "dts":
-	    case "ac3":
+            case "dts":
+            case "ac3":
                 return "audio";
             default:
                 return "file";
@@ -155,59 +170,141 @@
             re = /valign=center><a href='([\S\s]*?)'>([\S\s]*?)<b>([\S\s]*?)<\/b><\/a><.*?>([\S\s]*?)>\&nbsp\;/g;
             // 1 = link, 2 = raw image link or empty field, 3 = title, 4 = additional info
             var match = re.exec(response);
+
+            if (!match) { // try the page as texts
+                re = /class=include([\S\s]*?)<\/tr><\/table>/;
+                match = re.exec(response);
+                if (match) response = match[1];
+                // 1 = link, 2 = raw image link or empty field, 3 = title, 4 = additional info
+                re = /<tr><td><a href='([\S\s]*?)'>([\S\s]*?)<b>([\S\s]*?)<\/b><\/a><.*?>([\S\s]*?)>\&nbsp\;/g;
+                match = re.exec(response);
+            }
+
             if (!match) {
                 // check if that is megogo page
                 re = /class="button">[\S\s]*?<a href="([^\?]+)/;
                 match = re.exec(response);
                 if (match) {
-		    var re = /[\S\s]*?([\d+^\?]+)/i;
-        	    var videoID = re.exec(match[1])[1];
-        	    var sign = showtime.md5digest('video=' + videoID + 'megogosign123');
-        	    sign = showtime.JSONDecode(showtime.httpGet('http://megogo.net/p/video?video=' + videoID + '&sign=' + sign));
+                    var re = /[\S\s]*?([\d+^\?]+)/i;
+                    var videoID = re.exec(match[1])[1];
+                    var sign = showtime.md5digest('video=' + videoID + 'megogosign123');
+                    sign = showtime.JSONDecode(showtime.httpGet('http://megogo.net/p/video?video=' + videoID + '&sign=' + sign));
                     page.appendItem(PREFIX + ":playmego:" + match[1], "video", {
                         title: new showtime.RichText(page.metadata.title),
                         icon: 'http://megogo.net' + unescape(sign.video[0].image.big),
-			year: +parseInt(sign.video[0].year),
-			genre: unescape(sign.video[0].genre_list[0].title),
-			rating: sign.video[0].rating_imdb * 10,
-			duration: +parseInt(sign.video[0].duration),
-			description: unescape(sign.video[0].description)
+                        year: +parseInt(sign.video[0].year),
+                        genre: unescape(sign.video[0].genre_list[0].title),
+                        rating: sign.video[0].rating_imdb * 10,
+                        duration: +parseInt(sign.video[0].duration),
+                        description: unescape(sign.video[0].description)
                     });
                     url = 0;
                     return false;
                 }
-                // Scraping the page as a folder
-		re = /valign=top>[\S\s]*?<img src='([^\']+)[\S\s]*?<\/small>([\S\s]*?)\&nbsp\;/;
+                // Scraping the page as a folder 
+                // 1 - img link, 2 - description
+                re = /valign=top>[\S\s]*?<img src='([^\']+)[\S\s]*?<\/small>([\S\s]*?)<\/td>/;
                 match = re.exec(response);
-		if (match) {
-			page.appendPassiveItem("video",  "", {
-  				title: page.metadata.title,
-   				icon: match[1],
-				description: new showtime.RichText(match[2].replace(/(\r\n|\n|\r)/gm,""))
-			});
-		};
+                if (match) {
+                    if (match[2].indexOf("\&nbsp\;") > 0) match[2] = match[2].substring(0, match[2].indexOf("\&nbsp\;"));
+                    page.appendPassiveItem("video", "", {
+                        title: page.metadata.title,
+                        icon: match[1],
+                        description: new showtime.RichText(match[2].replace(/(\r\n|\n|\r)/gm, ""))
+                    });
+                };
+
+                // Try to handle a page as a artist's page
+                re = /<div class="poster">[\S\s]*?<img src="([^"]+)" \/>[\S\s]*?<div id="content_page">([\S\s]*?)<\/div>/g;
+                match = re.exec(response);
+                var artist = 0;
+                if (match) {
+                    artist = 1;
+                    re = /playlist: \[ "([^"]+)/;
+                    var match2 = re.exec(response);
+                    if (match2) {
+                        page.appendItem(match2[1], "video", {
+                            title: page.metadata.title,
+                            icon: match[1],
+                            description: new showtime.RichText(match[2].replace(/(\r\n|\n|\r)/gm, ""))
+                        });
+                    } else {
+                        page.appendPassiveItem("video", "", {
+                            title: page.metadata.title,
+                            icon: match[1],
+                            description: new showtime.RichText(match[2].replace(/(\r\n|\n|\r)/gm, ""))
+                        });
+
+                    }
+                    re = /<a class="active" href="[^"]+"><b>[\S\s]*?<\/b><\/a>([\S\s]*?)<!-/;
+                    match2 = re.exec(response);
+                    if (match2[1]) {
+                        re = /<a href="([^"]+)"><b>([^<]+)<\/b>/g;
+                        var match3 = re.exec(match2[1]);
+                        while (match3) {
+                            page.appendItem(PREFIX + ':index:' + match3[1], "directory", {
+                                title: match3[2],
+                                icon: logo
+                            });
+                            match3 = re.exec(match2[1]);
+                        }
+                    }
+                }
                 // 1 - link, 2 - title		
                 re = /i_disk.jpg[\S\s]*?<a href='([^\']+)' title='([\S\s]*?)'/g;
                 match = re.exec(response);
                 if (match) {
                     while (match) {
-			var v = "videoparams:" + showtime.JSONEncode({
-				sources: [{
-					url: BASE_URL + match[1]
-				}],
-				  title: showtime.entityDecode(match[2])
-				});
-			if (getType(match[2].split('.').pop()) != "video") v = BASE_URL + match[1];
-			page.appendItem(v, getType(match[2].split('.').pop()), {
-   				title: showtime.entityDecode(match[2]),
-   				icon: logo
-			});
+                        var v = "videoparams:" + showtime.JSONEncode({
+                            sources: [{
+                                url: BASE_URL + match[1]
+                            }],
+                            title: showtime.entityDecode(match[2])
+                        });
+                        if (getType(match[2].split('.').pop()) != "video") v = BASE_URL + match[1];
+                        page.appendItem(v, getType(match[2].split('.').pop()), {
+                            title: showtime.entityDecode(match[2]),
+                            icon: logo
+                        });
                         match = re.exec(response);
                     }
                     url = 0;
                     return false;
                 }
-                showtime.message("Error: Looks like the page is empty, sorry ;)", true, false);
+                if (artist) {
+                    url = 0;
+                    return false;
+                }
+		// Tops
+                //1-link 2-title 3 - additional info
+                re = /<tr><td><b style='[\S\s]*?<p><a href='([^']+)'><img src='[\S\s]*?<b>([\S\s]*?)<\/b><\/a>([\S\s]*?)<\/td>/g;
+                var match = re.exec(response);
+		if (match) {
+                while (match) {
+                    var title = "";
+                    var re2 = /class=info>([^<]+)/;
+                    if (re2.exec(match[3])) title += re2.exec(match[3])[1];
+                    re2 = /<small>([\S\s]*?)<\/small>[\S\s]*?<small>([\S\s]*?)<\/small>/;
+                    if (re2.exec(match[3])) {
+                        if (title) title += ", ";
+                        title += re2.exec(match[3])[2];
+                    }
+
+                    page.appendItem(PREFIX + ":index:" + match[1], "directory", {
+                        title: new showtime.RichText(match[2] + (title ? ('<font color="6699CC"> (' + (title) + ')</font>') : "")),
+                        icon: logo
+                    });
+                    page.entries++;
+                    match = re.exec(response);
+                };
+                re = /alt='перейти на следующую страницу[\S\s]*?<a href='([^']+)/;
+                url = re.exec(response);
+                if (!url) return false;
+                url = url[1];
+                return true;
+		};
+		
+                page.error('Похоже мы попали на пустую страницу / Looks like the page is empty, sorry :(');
                 return false;
             }
 
@@ -222,6 +319,7 @@
                 match[4] = match[4].replace("class=info>", "> ");
                 match[4] = match[4].replace(regex, "");
                 match[4] = match[4].replace(/(^\s*)|(\s*$)/gi, "");
+                if (match[4].indexOf("&nbsp;") == 0) match[4] = "";
                 page.appendItem(PREFIX + ":index:" + match[1], "directory", { //match[2] ? "video" : "directory", {
                     title: new showtime.RichText(match[3] + (match[4] ? ('<font color="6699CC"> (' + (match[4]) + ')</font>') : "")),
                     icon: icon
@@ -254,12 +352,21 @@
                 });
                 var re = /<title>([\S\s]*?)<\/title>/;
                 setPageHeader(page, re.exec(response)[1]);
-                //1-link 2-title
-                re = /<tr><td><a href='([^']+)'[\S\s]*?<b>([\S\s]*?)<\/b><\/a>/g;
+                //1-link 2-title 3 - additional info
+                re = /<tr><td><a href='([^']+)'><img src='[\S\s]*?<b>([\S\s]*?)<\/b><\/a>([\S\s]*?)<\/td>/g;
                 var match = re.exec(response);
                 while (match) {
+                    var title = "";
+                    var re2 = /class=info>([^<]+)/;
+                    if (re2.exec(match[3])) title += re2.exec(match[3])[1];
+                    re2 = /<small>([\S\s]*?)<\/small>[\S\s]*?<small>([\S\s]*?)<\/small>/;
+                    if (re2.exec(match[3])) {
+                        if (title) title += ", ";
+                        title += re2.exec(match[3])[2];
+                    }
+
                     page.appendItem(PREFIX + ":index:" + match[1], "directory", {
-                        title: new showtime.RichText(match[2]),
+                        title: new showtime.RichText(match[2] + (title ? ('<font color="6699CC"> (' + (title) + ')</font>') : "")),
                         icon: logo
                     });
                     page.entries++;
@@ -279,4 +386,3 @@
         }
     });
 })(this);
-
