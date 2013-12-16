d206058d99ebd47f7b8095604d5c44030d1bce8a
Лёнчик <lprot@list.ru>
2013-12-16 18:28:48 +0200

    modified:   plugins/bugtester.zip

--- bugtester/bugtester.js
+++ bugtester/bugtester.js
@@ -20,7 +20,7 @@
 (function(plugin) {
 
     var PREFIX = 'bugtester';
-    var BASE_URL = 'http://bugtester';
+    var BASE_URL = 'http://sdf.to';
 
     var logo = plugin.path + "logo.png";
 
@@ -50,23 +50,37 @@
 
     plugin.addURI(PREFIX + ":start", startPage);
 
-    plugin.addSearcher("bugtester", logo,
+    plugin.addSearcher("bugtester", logo, function(page, query) {
+        var fromPage = 1, tryToSearch = true;
+        //1-link, 2-title, 3-image, 4 - description, 5 - type, 6 - type in text, 7 - genre
+        var re = /class="image-wrap">[\S\s]*?<a href="([^"]+)" title="([^"]+)"><img src="([^"]+)[\S\s]*?<p class="text">([\S\s]*?)<\/p>[\S\s]*?<span class="section ([^"]+)">([\S\s]*?)<\/span>[\S\s]*?<span class="genre"><span class="caption">Жанр:<\/span><span>([\S\s]*?)<\/span>/g;
+        var re2 = /<b>Следующая страница<\/b>/;
 
-    function(page, query) {
-	page.type = "directory";
-        page.loading = false;
+        function loader() {
+            if (!tryToSearch) return false;
+	    var link = BASE_URL + "/search.aspx?search=" + query.replace(/\s/g, '\+') 
+            if (fromPage != 1) link = link + "&page=" + fromPage;
+            var response = showtime.httpGet(link);
+            var match = re.exec(response);
+            while (match) {
+				showtime.trace(match[2]);
+                page.appendItem(PREFIX + ":listRoot:" + escape(match[1]) + ":" + escape(match[2]), "video", {
+                    title: new showtime.RichText(match[2]),
+                    icon: match[3],
+                    genre: match[7],
+                    description: new showtime.RichText('Раздел: ' + blueStr(match[6]) + '\n' + match[4])
+                });
+                page.entries++;
+                match = re.exec(response);
+            };
 
-        var response = showtime.httpGet("http://www.google.com");
-	showtime.trace(response);
-
-        page.appendPassiveItem('video', {
-            title: 'Test'
-        });
-
-	showtime.trace("HELLO WORLD AGAIN !!!");
-
-        page.entries++;
-        return false;
+            if (!re2.exec(response)) return tryToSearch = false;
+            fromPage++;
+            return true;
+        };
+        loader();
+        page.paginator = loader;
     });
+
 })(this);
 
