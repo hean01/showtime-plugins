db2136b4f74195097e0f3a6d11e500ac5ac8fd98
lprot <lprot@list.ru>
2014-02-18 14:31:31 +0200

    added subs:   btvm/btvm.js 	ver bump:   btvm/plugin.json 	added subs:   plugins/btvm.zip

--- btvm/btvm.js
+++ btvm/btvm.js
@@ -22,10 +22,6 @@
     var BASE_URL = 'http://btvm.biz';
     var logo = plugin.path + "logo.png";
 
-    function fixNumEntities(s) {
-        return s.replace(/&#([^\s]*);/g, function(match, match2) { return String.fromCharCode(Number(match2)); });
-    }
-
     function setPageHeader(page, title) {
         page.loading = false;
         if (page.metadata) {
@@ -66,8 +62,9 @@
             var re = /<center><font size="6">([\s\S]*?)<\/font>[\s\S]*?<img src="([\s\S]*?)"[\s\S]*?<!--dle_image_end-->([\s\S]*?)<div style="[\s\S]*?<a href="([\s\S]*?)">/g;
             var match = re.exec(response);
             while (match) {
-                page.appendItem(PREFIX + ":indexItem:" + match[4], 'video', {
-                    title: fixNumEntities(showtime.entityDecode(match[1])),
+                var title = showtime.entityDecode(showtime.entityDecode(match[1]));
+                page.appendItem(PREFIX + ":indexItem:" + match[4] + ":" + escape(title), 'video', {
+                    title: title,
                     icon: match[2],
                     description: new showtime.RichText(match[3].replace(/<br>/,'').replace(/PLAY/,''))
                 });
@@ -82,9 +79,18 @@
         page.paginator = loader;
     });
 
-    plugin.addURI(PREFIX + ":indexItem:(.*)", function(page, url) {
+    // Search IMDB ID by title
+    function getIMDBid(title) {
+        var resp = showtime.httpGet('http://www.imdb.com/find?q=' + encodeURIComponent(showtime.entityDecode(unescape(title).replace(/3D\:/,''))).toString()).toString();
+        var imdbid = resp.match(/class="findResult[\S\s]*?<a href="\/title\/([\S\s]*?)\/\?/);
+        return imdbid ? imdbid[1] : '';
+    };
+
+    plugin.addURI(PREFIX + ":indexItem:(.*):(.*)", function(page, url, title) {
         var response = showtime.httpReq(url).toString();
-        setPageHeader(page, response.match(/<title>([\S\s]*?)<\/title>/)[1]);
+        setPageHeader(page, response.match(/<title>([\S\s]*?)<\/title>/)[1] + ' - ' +unescape(title));
+        var imdbid = getIMDBid(unescape(title));
+        //showtime.print(imdbid);
         page.loading = true;
         // Try to parse as RUSS
         var re = /<a id="boxplus002" href="([\s\S]*?)"/g;
@@ -92,13 +98,20 @@
         var added = 0;
         while (match) {
             if (match[1].indexOf("download") != -1) {
-                var title = match[1].replace(/http:\/\/btvm.biz\/[\S\s]*?\/download.php\?mov=\/[\S\s]*?\//, '');
-                title = title.replace(/http:\/\/btvm.biz\/[\S\s]*?\/download.php\?mov=lib\/[\S\s]*?\//, '');
-                title = title.replace(/.html/, '');
-                title = title.replace(/<\/b>/, '');
-                title = title.replace(/.rar/, '');
-                page.appendItem(match[1].replace(/<\/b>/,''), 'video', {
-                    title: title
+                var itemTitle = match[1].replace(/http:\/\/btvm.biz\/[\S\s]*?\/download.php\?mov=\/[\S\s]*?\//, '');
+                itemTitle = itemTitle.replace(/http:\/\/btvm.biz\/[\S\s]*?\/download.php\?mov=lib\/[\S\s]*?\//, '');
+                itemTitle = itemTitle.replace(/.html/, '');
+                itemTitle = itemTitle.replace(/<\/b>/, '');
+                itemTitle = itemTitle.replace(/.rar/, '');
+                page.appendItem("videoparams:" + showtime.JSONEncode({
+                        sources: [{
+                            url: match[1].replace(/<\/b>/, '')
+                        }],
+                        title: unescape(title),
+                        canonicalUrl: PREFIX + ":" + itemTitle,
+                        imdbid: imdbid
+                    }), 'video', {
+                    title: itemTitle
                 });
                 added++;
             }
@@ -109,8 +122,15 @@
             var re = /bold;"><a href="([\S\s]*?)" target=[\S\s]*?pointer;">([\S\s]*?)</g;
             var match = re.exec(response);
             while (match) {
-                page.appendItem(match[1], 'video', {
-                    title: match[2].replace(/Download /, '')
+                page.appendItem("videoparams:" + showtime.JSONEncode({
+                        sources: [{
+                            url: match[1]
+                        }],
+                        title: unescape(title),
+                        canonicalUrl: match[1],
+                        imdbid: imdbid
+                   }), 'video', {
+                   title: match[2].replace(/Download /, '')
                 });
                 added++;
                 match = re.exec(response);
