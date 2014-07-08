69567c8980773919de3087d78dea3643ded4e8eb
lprot <lprot@list.ru>
2014-07-08 17:12:09 +0300

    modified:   plugins/yts_re.zip 	modified:   yts_re/plugin.json 	modified:   yts_re/yts.js

--- yts_re/yts.js
+++ yts_re/yts.js
@@ -23,6 +23,16 @@
     var logo = plugin.path + "logo.png";
     var slogan = 'Welcome to the official YTS website. Here you will be able to browse and download movies in excellent DVD, 720p, 1080p and 3D quality, all at the smallest file size.';
 
+    var blue = '6699CC', orange = 'FFA500', red = 'EE0000', green = '008B45';
+
+    function colorStr(str, color) {
+        return '<font color="' + color + '"> (' + str + ')</font>';
+    }
+
+    function coloredStr(str, color) {
+        return '<font color="' + color + '">' + str + '</font>';
+    }
+
     function setPageHeader(page, title) {
         if (page.metadata) {
             page.metadata.title = title;
@@ -50,6 +60,117 @@
         }
     });
 
+    plugin.addURI(PREFIX + ":trailer:(.*)", function(page, url) {
+        setPageHeader(page, '');
+        page.loading = true;
+        var doc = showtime.httpReq(unescape(url)).toString();
+        doc = unescape(doc.match(/"url_encoded_fmt_stream_map":([\s\S]*?):/)[1]);
+        showtime.print(doc);
+        var re = /url=([\s\S]*?)\,/g;
+        var match = re.exec(doc);
+        while (match) {
+//            showtime.print(match);
+            page.appendItem(unescape(match[1]), "video", {
+                title: 'Trailer'
+            });
+            match = re.exec(doc);
+        }
+                page.loading = false;
+return;
+        page.loading = false;
+            if (match) {
+                page.type = "video";
+                page.source = "videoparams:" + showtime.JSONEncode({
+//                    title: unescape(title),
+                    sources: [{
+                        url: "hls:" + match[1].replace(/\\\//g, '/')
+                    }]
+                });
+            } else
+                 page.error("Sorry, can't get channel's link :(");
+
+
+    });
+
+    plugin.addURI(PREFIX + ":movie:(.*)", function(page, id) {
+        page.loading = true;
+        var json = showtime.JSONDecode(showtime.httpReq(BASE_URL + 'movie.json?id=' + id));
+        setPageHeader(page, json.MovieTitle);
+
+        page.appendItem('torrent:video:' + json.TorrentUrl, "video", {
+               title: new showtime.RichText(json.MovieTitleClean + colorStr(json.Size, blue)),
+               year: +json.MovieYear,
+               duration: json.MovieRuntime * 60,
+               rating: +json.MovieRating * 10,
+               icon: json.LargeCover,
+               genre: json.Genre1 + ', ' + json.Genre2,
+               description: new showtime.RichText(coloredStr('Seeds: ', orange) + coloredStr(json.TorrentSeeds, green) + coloredStr(' Peers: ', orange) + coloredStr(json.TorrentPeers, red) + ' ' +
+                   coloredStr('Uploaded: ', orange) + json.DateUploaded +
+                   coloredStr(' By: ', orange) + json.Uploader +
+                   (json.UploaderNotes ? coloredStr(' Notes: ', orange) + json.UploaderNotes : '') +
+                   coloredStr('<br>Language: ', orange) + json.Language + coloredStr(' Subtitles: ', orange) + json.Subtitles +
+                   coloredStr('<br>Age rating: ', orange) + json.AgeRating +
+                   coloredStr(' Downloaded: ', orange) + json.Downloaded +
+                   coloredStr(' Resolution: ', orange) + json.Resolution + 'x' + json.FrameRate + 'fps' +
+                   coloredStr(' Size: ', orange) + json.Size +
+                   coloredStr('<br>Description: ', orange) + json.LongDescription)
+        });
+        page.appendItem(PREFIX+':trailer:'+json.YoutubeTrailerUrl, "video", {
+            title: 'Trailer'
+        });
+        page.appendItem(json.LargeCover, "image", {
+            title: 'Cover'
+        });
+        page.appendItem(json.LargeScreenshot1, "image", {
+            title: 'Screenshot1'
+        });
+        page.appendItem(json.LargeScreenshot2, "image", {
+            title: 'Screenshot2'
+        });
+        page.appendItem(json.LargeScreenshot3, "image", {
+            title: 'Screenshot3'
+        });
+
+        page.appendItem("", "separator", {
+	    title: 'Actors:'
+	});
+        for (var i in json.CastList) {
+            page.appendItem(PREFIX + ':list:' + escape(json.CastList[i].ActorName), "directory", {
+                title: json.CastList[i].ActorName + ' as ' + json.CastList[i].CharacterName
+            });
+        }
+
+        page.appendItem("", "separator", {
+	    title: 'Directors:'
+	});
+        for (var i in json.DirectorList) {
+            page.appendItem(PREFIX + ':list:' + escape(json.DirectorList[i].DirectorName), "directory", {
+                title: json.DirectorList[i].DirectorName
+            });
+        }
+
+        page.loading = true;
+        var json = showtime.JSONDecode(showtime.httpReq(BASE_URL + 'comments.json?movieid=' + id));
+        page.loading = false;
+
+        var first = true;
+        for (var i in json) {
+            if (first) {
+                page.appendItem("", "separator", {
+	            title: 'Comments:'
+           	});
+                first = false;
+            };
+            page.appendPassiveItem('video', '', {
+                title: new showtime.RichText(coloredStr(json[i].UserName, orange) + ' (' + json[i].DateAdded + ')'),
+                icon: json[i].UserAvatar,
+                description: new showtime.RichText(json[i].CommentText)
+            });
+        }
+
+
+     });
+
     function browseItems(page, query) {
         var offset = 1;
         page.entries = 0;
@@ -64,9 +185,9 @@
                 }, query]
             }));
 
-            for(var i in c.MovieList) {
+            for (var i in c.MovieList) {
                 var mov = c.MovieList[i];
-                var item = page.appendItem("torrent:video:" + mov.TorrentUrl, "video", {
+                var item = page.appendItem(PREFIX + ':movie:' + mov.MovieID, "video", {
                     title: mov.MovieTitleClean,
                     icon: mov.CoverImage
                 });
