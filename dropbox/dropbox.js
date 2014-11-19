53302355bbda105ce05177c6c7ee523cf2c134f6
lprot <lprot@list.ru>
2014-11-19 23:59:19 +0200

    modified:   plugins/dropbox.zip

--- dropbox/dropbox.js
+++ dropbox/dropbox.js
@@ -45,6 +45,8 @@
         var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
         if (bytes == 0) return '0 Byte';
         var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+        showtime.trace(bytes + ' ' + i + ' ' + Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]);
+        showtime.trace(+bytes + ' ' + i + ' ' + Math.round(+bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]);
         return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
     };
 
