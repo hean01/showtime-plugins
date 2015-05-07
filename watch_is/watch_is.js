2d5124bc5174c58584a83a420210cac150ceaa91
lprot <lprot@list.ru>
2015-05-07 07:47:01 +0300

    migrated to uppod links:   plugins/watch_is.zip 	ver bump:   watch_is/plugin.json 	migrated to uppod links:   watch_is/watch_is.js

--- watch_is/watch_is.js
+++ watch_is/watch_is.js
@@ -134,11 +134,50 @@
         return imdbid;
     };
 
+    function base64_decode(data) { // http://kevin.vanzonneveld.net
+        var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+        var o1, o2, o3, h1, h2, h3, h4, bits, i = 0, ac = 0, dec = "", tmp_arr = [];
+        if (!data)
+            return data;
+        data += '';
+        do { // unpack four hexets into three octets using index points in b64
+            h1 = b64.indexOf(data.charAt(i++));
+            h2 = b64.indexOf(data.charAt(i++));
+            h3 = b64.indexOf(data.charAt(i++));
+            h4 = b64.indexOf(data.charAt(i++));
+            bits = h1 << 18 | h2 << 12 | h3 << 6 | h4;
+            o1 = bits >> 16 & 0xff;
+            o2 = bits >> 8 & 0xff;
+            o3 = bits & 0xff;
+            if (h3 == 64)
+                tmp_arr[ac++] = String.fromCharCode(o1);
+            else if (h4 == 64)
+                tmp_arr[ac++] = String.fromCharCode(o1, o2);
+            else
+                tmp_arr[ac++] = String.fromCharCode(o1, o2, o3);
+        } while (i < data.length);
+        dec = tmp_arr.join('');
+        return dec;
+    }
+
+    function unhash(hash) {
+        var hash1 = "aG9w1NlTIR72nB4H3U0pYcix8q",
+            hash2 = "Zv6WmygXboVdktMQu5DeJszfL=";
+
+        for (var i = 0; i < hash1.length; i++) {
+            hash = hash.split(hash1[i]).join('--');
+            hash = hash.split(hash2[i]).join(hash1[i]);
+            hash = hash.split('--').join(hash2[i]);
+        }
+        return base64_decode(hash);
+    }
+
     // Play links
     plugin.addURI(PREFIX + ":video:(.*):(.*)", function(page, url, title) {
 	if (!setPageHeader(page, unescape(title))) return;
 	page.loading = true;
         var imdbid = getIMDBid(title);
+	var v = showtime.httpReq(BASE_URL + unescape(url)).toString();
 
 	function addItem(link, type) {
 	    var vparams = "videoparams:" + showtime.JSONEncode({
@@ -151,42 +190,45 @@
 		}]
 	    });
 	    page.appendItem(vparams, 'video', {
-		title: new showtime.RichText(coloredStr(type, blue) + " " + v.match(/<title>([\S\s]*?)<\/title>/)[1]),
-		icon: v.match(/<poster>([\S\s]*?)<\/poster>/)[1],
-		genre: v.match(/<genre>([\S\s]*?)<\/genre>/)[1],
-		year: parseInt(v.match(/<year>([\S\s]*?)<\/year>/)[1]),
-		duration: parseInt(v.match(/<duration>([\S\s]*?)<\/duration>/)[1]),
-		description: new showtime.RichText(coloredStr("Страна: ", orange)+v.match(/<country>([\S\s]*?)<\/country>/)[1] +
-		    coloredStr(" Режиссер: ", orange) + v.match(/<director>([\S\s]*?)<\/director>/)[1] +
-		    coloredStr(" В ролях: ", orange) + trim(v.match(/<cast>([\S\s]*?)<\/cast>/)[1]) + '\n\n' +
-		    coloredStr("Описание: ", orange) + v.match(/<about>([\S\s]*?)<\/about>/)[1])
+		title: new showtime.RichText(coloredStr(type, blue) + " " + v.match(/Название:<\/span>([\S\s]*?)<br \/>/)[1]),
+		icon: BASE_URL + v.match(/<div class="preview">[\S\s]*?<img src="([\S\s]*?)"/)[1],
+		genre: v.match(/Жанр:<\/span> <a href="[\S\s]*?">([\S\s]*?)<\/a>/)[1],
+		year: parseInt(v.match(/Год:<\/span> <a href="[\S\s]*?">([\S\s]*?)<\/a>/)[1]),
+		duration: parseInt(v.match(/Длительность:<\/span>([\S\s]*?)мин<br \/>/)[1]),
+		description: new showtime.RichText(coloredStr("Студия/Страна: ", orange) + v.match(/Студия\/Страна:<\/span>([\S\s]*?)<br \/>/)[1] +
+		    coloredStr(" Режиссер: ", orange) + v.match(/Режиссер:<\/span>([\S\s]*?)<br \/>/)[1] +
+		    coloredStr(" В ролях: ", orange) + trim(v.match(/В ролях:<\/span>([\S\s]*?)<br \/>/)[1]) + '\n' +
+                    coloredStr(" Перевод: ", orange) + trim(v.match(/Перевод:<\/span>([\S\s]*?)<br \/>/)[1]) + '\n' +
+		    coloredStr("Описание: ", orange) + v.match(/О фильме:<\/span><br \/>([\S\s]*?)<br \/>/)[1])
 	    });
 	    page.loading = false;
 	}
-        var v = showtime.httpReq(BASE_URL + '/api/watch/' + unescape(url).match(/[\S\s]*?([\d+]+)/i)[1]).toString();
-        //showtime.print(v);
-        var tmp = v.match(/<hdhls>([\S\s]*?)<\/hdhls>/);
-	if (tmp)
-            addItem(tmp[1], 'HD HLS');
 
-        tmp = v.match(/<hdvideo>([\S\s]*?)<\/hdvideo>/);
-	if (tmp)
-            addItem(tmp[1], 'HD MP4');
+        var flashvars = v.match(/var flashvars =([\S\s]*?);/)
+        if (flashvars) {
+            flashvars = showtime.JSONDecode(flashvars[1]);
 
-        tmp = v.match(/<hls>([\S\s]*?)<\/hls>/);
-	if (tmp)
-            addItem(tmp[1], 'SD HLS');
+            var hls = unhash(flashvars.file);
+            //showtime.print(hls);
+            var audio = hls.match(/&audioIndex=\{([\S\s]*?)\}/);
+            if (audio) {
+               var audio = audio[1].split(';');
+               hls = hls.match(/([\S\s]*?)&/)[1];
+               for (var i in audio)
+                   addItem('hls:' + hls + '&audioIndex=' + audio[i], 'HLS' + audio[i]);
+            } else
+               addItem('hls:' + hls, 'HLS');
 
-        tmp = v.match(/<video>([\S\s]*?)<\/video>/);
-	if (tmp)
-            addItem(tmp[1], 'SD MP4');
-
-	var html = showtime.httpReq(BASE_URL + unescape(url)).toString();
+            var mp4 = showtime.JSONDecode(unhash(flashvars.st));
+            if (mp4.cntrl_my3 && mp4.cntrl_my3.link)
+               addItem(mp4.cntrl_my3.link, 'MP4');
+            //showtime.print(mp4.cntrl_my3.link);
+        }
 	page.loading = false;
 
 	// 1-icon, 2-nick, 3-age, 4-date/time, 5-comment
 	var re = /<div class="avatar"><a href="[\s\S]*?"><img src="([\s\S]*?)"[\s\S]*?<strong>([\s\S]*?)<\/strong>[\s\S]*?<div class="sex">([\s\S]*?)<\/span>[\s\S]*?<div class="date">([\s\S]*?)<\/div>[\s\S]*?<div class="comment" id="[\s\S]*?">([\s\S]*?)<\/div>/g;
-        var match = re.exec(html);
+        var match = re.exec(v);
 	var counter = 0;
 	while (match) {
 	    if (counter == 0) {
@@ -200,7 +242,7 @@
 		description: new showtime.RichText(match[5]),
 		icon: match[1][0] == "/" ?  BASE_URL + match[1] : match[1]
 	    });
-	    match = re.exec(html);
+	    match = re.exec(v);
 	};
     });
 
