1aa3c7519c5afa16c20d13c01454637de92055f5
lprot <lprot@list.ru>
2015-05-11 23:42:58 +0300

    added sample list:   plugins/tv.zip 	ver bump:   tv/plugin.json 	added sample list:   tv/tv.js

--- tv/tv.js
+++ tv/tv.js
@@ -39,59 +39,29 @@
         return '<font color="' + color + '">' + str + '</font>';
     }
 
-    function base64_decode(data) {
-        // http://kevin.vanzonneveld.net
-        // +   original by: Tyler Akins (http://rumkin.com)
-        // +   improved by: Thunder.m
-        // +      input by: Aman Gupta
-        // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-        // +   bugfixed by: Onno Marsman
-        // +   bugfixed by: Pellentesque Malesuada
-        // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-        // +      input by: Brett Zamir (http://brett-zamir.me)
-        // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-        // *     example 1: base64_decode('S2V2aW4gdmFuIFpvbm5ldmVsZA==');
-        // *     returns 1: 'Kevin van Zonneveld'
-        // mozilla has this native
-        // - but breaks in 2.0.0.12!
-        //if (typeof this.window['atob'] == 'function') {
-        //    return atob(data);
-        //}
+    function base64_decode(data) { // http://kevin.vanzonneveld.net
         var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-        var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
-            ac = 0,
-            dec = "",
-            tmp_arr = [];
-
-        if (!data) {
+        var o1, o2, o3, h1, h2, h3, h4, bits, i = 0, ac = 0, dec = "", tmp_arr = [];
+        if (!data)
             return data;
-        }
-
         data += '';
-
         do { // unpack four hexets into three octets using index points in b64
             h1 = b64.indexOf(data.charAt(i++));
             h2 = b64.indexOf(data.charAt(i++));
             h3 = b64.indexOf(data.charAt(i++));
             h4 = b64.indexOf(data.charAt(i++));
-
             bits = h1 << 18 | h2 << 12 | h3 << 6 | h4;
-
             o1 = bits >> 16 & 0xff;
             o2 = bits >> 8 & 0xff;
             o3 = bits & 0xff;
-
-            if (h3 == 64) {
+            if (h3 == 64)
                 tmp_arr[ac++] = String.fromCharCode(o1);
-            } else if (h4 == 64) {
-                tmp_arr[ac++] = String.fromCharCode(o1, o2);
-            } else {
-                tmp_arr[ac++] = String.fromCharCode(o1, o2, o3);
-            }
+            else if (h4 == 64)
+                    tmp_arr[ac++] = String.fromCharCode(o1, o2);
+                 else
+                    tmp_arr[ac++] = String.fromCharCode(o1, o2, o3);
         } while (i < data.length);
-
         dec = tmp_arr.join('');
-
         return dec;
     }
 
@@ -106,9 +76,13 @@
         return base64_decode(hash);
     }
 
-    plugin.createService(slogan, plugin.getDescriptor().id + ":start", "tv", true, logo);
+    var service = plugin.createService(slogan, plugin.getDescriptor().id + ":start", "tv", true, logo);
 
     var settings = plugin.createSettings(slogan, logo, slogan);
+
+    settings.createBool('disableSampleList', "Don't show Sample list", false, function(v) {
+        service.disableSampleList = v;
+    });
 
     settings.createAction("cleanFavorites", "Clean My Favorites", function () {
         store.list = "[]";
@@ -1042,8 +1016,14 @@
                 }
             }
         });
-        showPlaylist(page);
 
+        if (!service.disableSampleList) {
+            var item = page.appendItem('m3u:http%3A%2F%2Fbit.ly%2F1Hbuve6:Sample list', "directory", {
+                title: 'Sample list'
+            });
+        }
+
+        showPlaylist(page);
 
         page.appendItem("", "separator", {
             title: 'Providers'
