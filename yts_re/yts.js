9c9b00fa975265f54836958464791bf02d7f958e
lprot <lprot@list.ru>
2015-11-15 02:31:50 +0200

    api fixes:   plugins/yts_re.zip 	ver bump:   yts_re/plugin.json 	api fixes:   yts_re/yts.js

--- yts_re/yts.js
+++ yts_re/yts.js
@@ -59,35 +59,6 @@
         service.baseUrl = v;
     });
 
-    settings.createMultiOpt('filter', "Filter quality by", [
-        ['ALL', 'All', true],
-        ['1080p', '1080p'],
-        ['720p', '720p'],
-        ['3D', '3D']
-        ], function(v) {
-            service.quality = v;
-    });
-
-    settings.createMultiOpt('sorting', "Sort results by", [
-        ['date_added', 'Date', true],
-        ['seeds', 'Seeds'],
-        ['peers', 'Peers'],
-        ['like_count', 'Like Count'],
-        ['title', 'Title'],
-        ['rating', 'Rating'],
-        ['downloaded_count', 'Downloaded Count'],
-        ['year', 'Year']
-        ], function(v) {
-            service.sorting = v;
-    });
-
-    settings.createMultiOpt('order', "Order by", [
-        ['desc', 'Descending', true],
-        ['asc', 'Ascending']
-        ], function(v) {
-            service.order = v;
-    });
-
     function concat(what) {
         var s = 0;
         for (var i in what)
@@ -98,7 +69,7 @@
     function concatEntity(entity, field) {
         var s = 0;
         for (var i in entity)
-            (s ? s+= ', ' + entity[i][field] : s = entity[i][field]);
+            (s ? s += ', ' + entity[i][field] : s = entity[i][field]);
         return s;
     }
 
@@ -130,7 +101,7 @@
             }
 
             page.loading = false;
-            if (offset == 1 && page.metadata)
+            if (offset == 1 && page.metadata && c.data.movie_count)
                page.metadata.title += ' (' + c.data.movie_count + ')';
 
             for (var i in c.data.movies) {
@@ -171,8 +142,72 @@
         page.loading = false;
     });
 
-    plugin.addURI(PREFIX + ":start", function(page) {
-        setPageHeader(page, plugin.getDescriptor().synopsis);
+    function constructMultiopt(multiOpt, storageVariable) {
+        var t = [
+            ['ALL', 'All'],
+            ['1080p', '1080p'],
+            ['720p', '720p'],
+            ['3D', '3D']
+        ];
+        if (!storageVariable)
+            multiOpt[0][2] = true;
+        else
+            for (var i = 0; i < multiOpt.length; i++) {
+                if (multiOpt[i][0] == storageVariable) {
+                    multiOpt[i][2] = true;
+                    break;
+                }
+            }
+        return multiOpt;
+    }
+
+    function addOptions(page) {
+        var optionsAreAdded = false;
+        showtime.print(service);
+        showtime.print(service.quality);
+        var options = constructMultiopt([['ALL', 'All'],
+                      ['1080p', '1080p'],
+                      ['720p', '720p'],
+                      ['3D', '3D']], service.quality);
+        page.options.createMultiOpt('filter', "Filter quality by", options, function(v) {
+                service.quality = v;
+                if (optionsAreAdded) {
+                    page.flush();
+                    processStartPage(page);
+                }
+        });
+
+        options = constructMultiopt([
+            ['date_added', 'Date'],
+            ['seeds', 'Seeds'],
+            ['peers', 'Peers'],
+            ['like_count', 'Like Count'],
+            ['title', 'Title'],
+            ['rating', 'Rating'],
+            ['downloaded_count', 'Downloaded Count'],
+            ['year', 'Year']], service.sorting);
+        page.options.createMultiOpt('sorting', "Sort results by", options, function(v) {
+                service.sorting = v;
+                if (optionsAreAdded) {
+                    page.flush();
+                    processStartPage(page);
+                }
+        });
+
+        options = constructMultiopt([
+            ['desc', 'Descending'],
+            ['asc', 'Ascending']], service.order);
+        page.options.createMultiOpt('order', "Order by", options, function(v) {
+                service.order = v;
+                if (optionsAreAdded) {
+                    page.flush();
+                    processStartPage(page);
+                }
+        });
+        optionsAreAdded = true;
+    }
+
+    function processStartPage(page) {
         page.appendItem(PREFIX + ":genres", "directory", {
             title: 'Genres'
         });
@@ -194,6 +229,12 @@
             sort: 'peers'
         });
         page.loading = false;
+    }
+
+    plugin.addURI(PREFIX + ":start", function(page) {
+        setPageHeader(page, plugin.getDescriptor().synopsis);
+        addOptions(page);
+        processStartPage(page);
     });
 
     plugin.addURI(PREFIX + ":list:(.*)", function(page, query) {
@@ -218,7 +259,7 @@
             icon: mov.medium_cover_image,
             year: +mov.year,
             rating: mov.rating * 10,
-            duration: mov.runtime,
+            duration: +mov.runtime,
             genre: concat(mov.genres),
             description: new showtime.RichText(coloredStr('Seeds: ', orange) + coloredStr(concatEntity(mov.torrents, 'seeds'), green) +
                 coloredStr('\nPeers: ', orange) + coloredStr(concatEntity(mov.torrents, 'peers'), red) + ' ' +
@@ -241,19 +282,19 @@
         page.loading = true;
         var json = showtime.JSONDecode(showtime.httpReq(service.baseUrl + '/api/v2/movie_details.json?movie_id=' + id + '&with_images=true&with_cast=true'));
         setPageHeader(page, json.data.title);
-        page.metadata.background = json.data.images.background_image;
-        page.metadata.backgroundAlpha = 0.3;
-        for (var i in json.data.torrents) {
-            var link = json.data.torrents[i].url.match(/(\/torrent\/download(.*))/);
+        if (page.metadata.background && json.data.background_image)
+           page.metadata.background = json.data.background_image;
+        //page.metadata.backgroundAlpha = 0.3;
+        for (var i in json.data.movie.torrents) {
+            var link = json.data.movie.torrents[i].url.match(/(\/torrent\/download(.*))/);
             if (link)
                 link = service.baseUrl.replace('/api/', '') + link[1];
             else
-                link = json.data.torrents[i].url;
-
+                link = json.movie.torrents[i].url;
             var vparams = "videoparams:" + showtime.JSONEncode({
-                title: json.data.title,
-                canonicalUrl: PREFIX + ':movie:' + id + ':' + json.data.torrents[i].quality,
-                imdbid: json.data.imdb_code, //getIMDBid(json.MovieTitle),
+                title: json.data.movie.title,
+                canonicalUrl: PREFIX + ':movie:' + id + ':' + json.data.movie.torrents[i].quality,
+                imdbid: json.data.movie.imdb_code, //getIMDBid(json.MovieTitle),
                 no_fs_scan: true,
                 sources: [{
                     url: 'torrent:video:' + link
@@ -261,94 +302,107 @@
 	    });
 
             page.appendItem(vparams, "video", {
-                 title: new showtime.RichText(coloredStr(json.data.torrents[i].quality, orange) + ' ' + json.data.title),
-                 year: +json.data.year,
-                 duration: json.data.runtime * 60,
-                 rating: +json.data.rating * 10,
-                 icon: json.data.images.medium_cover_image,
-                 genre: concat(json.data.genres),
-                 description: new showtime.RichText(coloredStr('Seeds: ', orange) + coloredStr(json.data.torrents[i].seeds, green) +
-                   coloredStr(' Peers: ', orange) + coloredStr(json.data.torrents[i].peers, red) + ' ' +
-                   coloredStr('Like Count: ', orange) + json.data.like_count +
-                   coloredStr('\nLanguage: ', orange) + json.data.language +
-                   coloredStr(' MPA rating: ', orange) + json.data.mpa_rating +
-                   coloredStr('\nDate Uploaded: ', orange) + json.data.torrents[i].date_uploaded +
-                   coloredStr(' Download Count: ', orange) + json.data.download_count +
-                   coloredStr('\nResolution: ', orange) + json.data.torrents[i].resolution + 'x' + json.data.torrents[i].framerate + 'fps' +
-                   coloredStr(' Size: ', orange) + json.data.torrents[i].size +
-                   coloredStr('<br>Description: ', orange) + json.data.description_full)
+                 title: new showtime.RichText(coloredStr(json.data.movie.torrents[i].quality, orange) + ' ' + json.data.movie.title),
+                 year: +json.data.movie.year,
+                 duration: json.data.movie.runtime * 60,
+                 rating: +json.data.movie.rating * 10,
+                 icon: json.data.movie.medium_cover_image,
+                 genre: concat(json.data.movie.genres),
+                 description: new showtime.RichText(coloredStr('Seeds: ', orange) + coloredStr(json.data.movie.torrents[i].seeds, green) +
+                   coloredStr(' Peers: ', orange) + coloredStr(json.data.movie.torrents[i].peers, red) + ' ' +
+                   (json.data.movie.like_count ? coloredStr('Like Count: ', orange) + json.data.movie.like_count : '') +
+                   coloredStr('\nLanguage: ', orange) + json.data.movie.language +
+                   (json.data.movie.mpa_rating ? coloredStr(' MPA rating: ', orange) + json.data.movie.mpa_rating : '') +
+                   coloredStr('\nDate Uploaded: ', orange) + json.data.movie.torrents[i].date_uploaded +
+                   (json.data.movie.download_count ? coloredStr(' Download Count: ', orange) + json.data.movie.download_count : '') +
+                   (json.data.movie.torrents[i].resolution ? coloredStr('\nResolution: ', orange) + json.data.movie.torrents[i].resolution + 'x' + json.data.movie.torrents[i].framerate + 'fps' : '') +
+                   coloredStr(' Size: ', orange) + json.data.movie.torrents[i].size +
+                   (json.data.movie.description_full ? coloredStr('<br>Description: ', orange) + json.data.movie.description_full : ''))
             });
         }
-        page.appendItem('youtube:video:'+escape(json.data.yt_trailer_code), "video", {
-            title: 'Trailer'
-        });
-        page.appendItem(json.data.images.large_cover_image, "image", {
-            title: 'Cover'
-        });
-        page.appendItem(json.data.images.large_screenshot_image1, "image", {
-            title: 'Screenshot1'
-        });
-        page.appendItem(json.data.images.large_screenshot_image2, "image", {
-            title: 'Screenshot2'
-        });
-        page.appendItem(json.data.images.large_screenshot_image3, "image", {
-            title: 'Screenshot3'
-        });
-
-        page.appendItem("", "separator", {
-	    title: 'Actors:'
-	});
-        for (var i in json.data.actors) {
-            page.appendItem(PREFIX + ':list:' + escape(json.data.actors[i].name), "video", {
-                title: json.data.actors[i].name + ' as ' + json.data.actors[i].character_name,
-                icon: json.data.actors[i].medium_image
+        if (json.data.movie.yt_trailer_code)
+            page.appendItem('youtube:video:'+escape(json.data.movie.yt_trailer_code), "video", {
+                title: 'Trailer'
             });
+        if (json.data.movie.large_cover_image)
+            page.appendItem(json.data.movie.large_cover_image, "image", {
+                title: 'Cover'
+            });
+        if (json.data.movie.large_screenshot_image1)
+            page.appendItem(json.data.movie.large_screenshot_image1, "image", {
+                title: 'Screenshot1'
+            });
+        if (json.data.movie.large_screenshot_image2)
+            page.appendItem(json.data.movie.large_screenshot_image2, "image", {
+                title: 'Screenshot2'
+            });
+        if (json.data.movie.large_screenshot_image3)
+            page.appendItem(json.data.movie.large_screenshot_image3, "image", {
+                title: 'Screenshot3'
+            });
+
+        if (json.data.movie.actors) {
+            page.appendItem("", "separator", {
+	        title: 'Actors:'
+            });
+            for (var i in json.data.movie.actors) {
+                page.appendItem(PREFIX + ':list:' + escape(json.data.actors[i].name), "video", {
+                    title: json.data.actors[i].name + ' as ' + json.data.actors[i].character_name,
+                    icon: json.data.actors[i].medium_image
+                });
+            }
         }
 
-        page.appendItem("", "separator", {
-	    title: 'Directors:'
-	});
-        for (var i in json.data.directors) {
-            page.appendItem(PREFIX + ':list:' + escape(json.data.directors[i].name), "video", {
-                title: json.data.directors[i].name,
-                icon: json.data.directors[i].medium_image
-            });
+        if (json.data.movie.directors) {
+            page.appendItem("", "separator", {
+                title: 'Directors:'
+	    });
+            for (var i in json.data.movie.directors) {
+                page.appendItem(PREFIX + ':list:' + escape(json.data.directors[i].name), "video", {
+                    title: json.data.directors[i].name,
+                    icon: json.data.directors[i].medium_image
+                });
+            }
         }
 
         // Suggestions
-        json = showtime.JSONDecode(showtime.httpReq(service.baseUrl + '/api/v2/movie_suggestions.json?movie_id=' + id));
-        var first = true;
-        for (var i in json.data.movie_suggestions) {
-            if (first) {
-                page.appendItem("", "separator", {
-	            title: 'Suggestions:'
-           	});
-                first = false;
-            };
-            addMovieItem(page, json.data.movie_suggestions[i]);
-        }
+        try {
+            json = showtime.JSONDecode(showtime.httpReq(service.baseUrl + '/api/v2/movie_suggestions.json?movie_id=' + id));
+            var first = true;
+            for (var i in json.data.movie_suggestions) {
+                if (first) {
+                    page.appendItem("", "separator", {
+	                title: 'Suggestions:'
+                    });
+                    first = false;
+                };
+                addMovieItem(page, json.data.movie_suggestions[i]);
+            }
+        } catch(err) {}
 
         // Comments
-        json = showtime.JSONDecode(showtime.httpReq(service.baseUrl + '/api/v2/movie_comments.json?movie_id=' + id));
-        first = true;
-        for (var i in json.data.comments) {
-            if (first) {
-                page.appendItem("", "separator", {
-	            title: 'Comments:'
-           	});
-                first = false;
-            };
-            page.appendPassiveItem('video', '', {
-                title: new showtime.RichText(coloredStr(json.data.comments[i].username, orange) + ' (' + json.data.comments[i].date_added + ')'),
-                icon: json.data.comments[i].medium_user_avatar_image,
-                description: new showtime.RichText(json.data.comments[i].comment_text +
-                   coloredStr('\nLike Count: ', orange) + json.data.comments[i].like_count)
-            });
-        }
+        try {
+            json = showtime.JSONDecode(showtime.httpReq(service.baseUrl + '/api/v2/movie_comments.json?movie_id=' + id));
+            first = true;
+            for (var i in json.data.comments) {
+                if (first) {
+                    page.appendItem("", "separator", {
+	                title: 'Comments:'
+                    });
+                    first = false;
+                };
+                page.appendPassiveItem('video', '', {
+                    title: new showtime.RichText(coloredStr(json.data.comments[i].username, orange) + ' (' + json.data.comments[i].date_added + ')'),
+                    icon: json.data.comments[i].medium_user_avatar_image,
+                    description: new showtime.RichText(json.data.comments[i].comment_text +
+                       coloredStr('\nLike Count: ', orange) + json.data.comments[i].like_count)
+                });
+           }
+        } catch(err) {}
         page.loading = false;
     });
 
-    plugin.addSearcher(plugin.getDescriptor().id, logo, function(page, query) {
+    plugin.addSearcher(plugin.getDescriptor().title, logo, function(page, query) {
         browseItems(page, {
             query_term: query
         });
